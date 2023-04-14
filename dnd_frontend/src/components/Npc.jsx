e8a94428c15298d02c3fd9a7301bf5e6540dcdc0
baileyrechkemmer <rechkemmer3@gmail.com>
2023-04-14 14:00:12 -0500

    app is at full mvp commiting before deploying

--- dnd_frontend/src/components/Npc.jsx
+++ dnd_frontend/src/components/Npc.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react'
 // css
+import './../css/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 // axios call / components
 import axios from 'axios'
 import Add from './Npc_add'
 import Edit from './Npc_edit'
 // bootstrap stuff
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
 import Card from 'react-bootstrap/Card'
 // import CardGroup from 'react-bootstrap/ListGroup'
 import Button from 'react-bootstrap/Button'
@@ -19,7 +18,7 @@ const Npc = (props) => {
 	// get npc function
 	const getNpc = () => {
 		axios
-			.get('http://localhost:8000/npc')
+			.get('https://dnd-backend-qq19.onrender.com/npc')
 			.then(
 				(response) => setNpc(response.data),
 				(err) => console.error(err)
@@ -29,17 +28,19 @@ const Npc = (props) => {
 
 	// create npc function
 	const handleCreate = (newNpc) => {
-		axios.post('http://localhost:8000/npc', newNpc).then((res) => {
-			console.log(res)
-			getNpc()
-		})
+		axios
+			.post('https://dnd-backend-qq19.onrender.com/npc', newNpc)
+			.then((res) => {
+				console.log(res)
+				getNpc()
+			})
 	}
 
 	// delete function
 
 	const handleDelete = (event) => {
 		axios
-			.delete(`http://localhost:8000/npc/${event.target.value}`)
+			.delete(`https://dnd-backend-qq19.onrender.com/npc/${event.target.value}`)
 			.then((response) => {
 				getNpc()
 			})
@@ -49,7 +50,7 @@ const Npc = (props) => {
 	const handleUpdate = (editNpc) => {
 		console.log(editNpc)
 		axios
-			.put(`http://localhost:8000/npc/${editNpc.id}`, editNpc)
+			.put(`https://dnd-backend-qq19.onrender.com/npc/${editNpc.id}`, editNpc)
 			.then((res) => {
 				getNpc()
 			})
@@ -64,40 +65,38 @@ const Npc = (props) => {
 			<h1>Npc Tracker</h1>
 			<Add handleCreate={handleCreate} />
 			<div className='npc'>
-				<Row class='g-5'>
+				
 					{npc.map((npc) => {
 						return (
-							
-								<Card style={{ width: '23rem' }}>
-									<Card.Img variant='top' src='holder.js/100px160' />
-									<Card.Body>
-										<Card.Title>{npc.name}</Card.Title>
-										<Card.Text>
-											Age: {npc.age}
-											<br />
-											quirk: {npc.quirk}
-											<br />
-											race: {npc.race}
-											<br />
-											alignment: {npc.alignment}
-											<br />
-											job: {npc.job}
-											<br />
-											Description: {npc.description}
-										</Card.Text>
-									</Card.Body>
-									<Card.Footer>
-										<Edit handleUpdate={handleUpdate} npc={npc} />
-									</Card.Footer>
-									{/* <Button onClick={() => props.onFormSwitch('detail')}>show npc</Button> */}
-									<Button onClick={handleDelete} value={npc.id}>
+							<Card style={{ width: '23rem' }}>
+								<Card.Body className='npcCards'>
+									<Card.Title>{npc.name}</Card.Title>
+									<Card.Text>
+										Age: {npc.age}
+										<br />
+										quirk: {npc.quirk}
+										<br />
+										race: {npc.race}
+										<br />
+										alignment: {npc.alignment}
+										<br />
+										job: {npc.job}
+										<br />
+										Description: {npc.description}
+									</Card.Text>
+									<Edit handleUpdate={handleUpdate} npc={npc} />
+									<br />
+									<br />
+									<Button className="delete button"onClick={handleDelete} value={npc.id}>
 										delete npc
 									</Button>
-								</Card>
+								</Card.Body>
+								{/* <Button onClick={() => props.onFormSwitch('detail')}>show npc</Button> */}
+							</Card>
 						)
 					})}
 					
-				</Row>
+				
 			</div>
 		</div>
 	)
